d32785a927df9f7d1b74ff226e2c28d53135d2af
Hannahli6 <hannahli6060@gmail.com>
2025-06-30 00:56:10 -0700

    fix: ensure JWT secret is defined before generating token

--- backend/src/lib/utlis.ts
+++ backend/src/lib/utlis.ts
@@ -2,15 +2,20 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import {Response} from "express";
 
-export const generateToken = (userId, res: Response) => {
+export const generateToken = (userId : String, res: Response) => {
 
   // First generate a token using JWT secret
+  const jwtSecret = process.env.JWT_SECRET;
+  if (!jwtSecret) {
+    throw new Error("JWT secret is not defined in environment variables");
+  }
+
   dotenv.config();
-  const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+  const token = jwt.sign({userId}, jwtSecret, {
     expiresIn:"7d",
   })
 
-  // Then we send it to a user with a cook (HTTPS COOKIE) expires in 7 days (the user will have to login again after 7 days)
+  // Then we send it to a user with a cookie (HTTPS COOKIE) expires in 7 days (the user will have to login again after 7 days)
   res.cookie("token", token,  {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks from cross site scripting attacks
